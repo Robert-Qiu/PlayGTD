6824f5dab7489116fed7c1ae2a00474f0d50d15b
robert.qiu <qwjcool@hotmail.com>
2016-05-28 00:31:59 +0800

    remove some UT for previous version

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -12,7 +12,7 @@ class ApplicationSpec extends PlaySpec with OneAppPerTest {
   "Routes" should {
 
     "send 404 on a bad request" in  {
-      route(app, FakeRequest(GET, "/boum")).map(status(_)) mustBe Some(NOT_FOUND)
+      // route(app, FakeRequest(GET, "/boum")).map(status(_)) mustBe Some(NOT_FOUND)
     }
 
   }
@@ -21,10 +21,10 @@ class ApplicationSpec extends PlaySpec with OneAppPerTest {
 
     "render the index page" in {
       val home = route(app, FakeRequest(GET, "/")).get
-
+/*
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Your new application is ready.")
+      contentAsString(home) must include ("Your new application is ready.") */
     }
 
   }
@@ -32,9 +32,11 @@ class ApplicationSpec extends PlaySpec with OneAppPerTest {
   "CountController" should {
 
     "return an increasing count" in {
+      /*
       contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "0"
-      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "1"
-      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "2"
+       */
+      /*contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "1"
+      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "2"*/
     }
 
   }
